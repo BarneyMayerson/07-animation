82b70d56877b09b9b1350971f4ff33b09885d791
Barney Mayerson <mr.barney.mayerson@gmail.com>
2023-07-04 10:29:15 +0300

    07-001 Animating with CSS Transitions

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,36 @@
+<template>
+  <button type="button" @click="flag = !flag">Toggle</button>
+
+  <transition name="fade">
+    <h2 v-if="flag">Hello there...</h2>  
+  </transition>
+</template>
+
 <script>
+export default {
+  name: "App",
+
+  data() {
+    return {
+      flag: false,
+    }
+  },
+}
 </script>
 
-<template>
-  
-</template>
+<style scoped>
+.fade-enter-from {
+  opacity: 0;
+}
+
+.fade-enter-active {
+  transition: all 1s linear;
+}
+
+.fade-leave-to {
+  transition: all .5s linear;
+  opacity: 0;
+}
+</style>
+
+
